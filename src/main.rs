8691cb27f7a48ff880ca83db01f2c1b8f4b49fff
sandriine <neveusandrine@free.fr>
2024-07-19 15:01:54 +0200

    tests actix

--- src/main.rs
+++ src/main.rs
@@ -21,3 +21,37 @@ async fn main() -> std::io::Result<()> {
     .run()
     .await
 }
+
+
+#[cfg(test)]
+mod tests {
+    use actix_web::{http::header::ContentType, test, App};
+
+    use super::*;
+
+    #[actix_web::test]
+    async fn test_hello_get() {
+        let app = test::init_service(App::new().service(hello)).await;
+        let req = test::TestRequest::default()
+            .insert_header(ContentType::plaintext())
+            .to_request();
+        let resp = test::call_service(&app, req).await;
+        assert!(resp.status().is_success());
+    }
+
+    #[actix_web::test]
+    async fn test_echo_post() {
+        let app = test::init_service(App::new().service(echo)).await;
+        let req = test::TestRequest::post().uri("/echo").to_request();
+        let resp = test::call_service(&app, req).await;
+        assert!(resp.status().is_success());
+    }
+
+        #[actix_web::test]
+        async fn test_echo_post_error() {
+            let app = test::init_service(App::new().service(echo)).await;
+            let req = test::TestRequest::post().uri("/").to_request();
+            let resp = test::call_service(&app, req).await;
+            assert!(resp.status().is_client_error());
+        }
+}
