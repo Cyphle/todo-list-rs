c1691d304cf1d972b1ba7859ee2dc5f04d06b6c9
Cyphle <cyril.phamle@gmail.com>
2024-07-26 17:29:41 +0200

    reset diesel

--- migrations/2024-07-26-122609_init-todo-list/down.sql
+++
@@ -1,9 +0,0 @@
--- Drop the index
-DROP INDEX IF EXISTS todo_list_item_idx;
-
--- Drop the foreign key constraint
-ALTER TABLE IF EXISTS todo_list_item DROP CONSTRAINT IF EXISTS fk_todo_list_item_to_do_list_id;
-
--- Drop the tables
-DROP TABLE IF EXISTS todo_list_item;
-DROP TABLE IF EXISTS todo_list;
