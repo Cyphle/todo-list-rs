45edfc405c00e4373e5d523e52973c5b7c5813ff
Cyphle <cyril.phamle@gmail.com>
2024-08-08 16:30:32 +0200

    another migration

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,6 +1,7 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20240808_132520_create_table;
+mod m20240808_142724_create_todo_list;
 
 pub struct Migrator;
 
@@ -9,6 +10,7 @@ impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
             Box::new(m20240808_132520_create_table::Migration),
+            Box::new(m20240808_142724_create_todo_list::Migration),
         ]
     }
 }
