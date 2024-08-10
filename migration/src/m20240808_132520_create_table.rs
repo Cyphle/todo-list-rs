ddc48f432dc2f00157e4629b598287444deecc53
DSanoussy <sanoussy.dansso@stonal.com>
2024-08-10 23:48:11 +0200

    fix "no such file or folder" error + add tests

--- migration/src/m20240808_132520_create_table.rs
+++ migration/src/m20240808_132520_create_table.rs
@@ -1,4 +1,5 @@
-use sea_orm_migration::{prelude::*, schema::*};
+use sea_orm_migration::{prelude::*};
+use crate::setup::{DOWN_SQL_DIR, UP_SQL_DIR, execute_sql_scripts};
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
@@ -6,26 +7,29 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        manager
-            .create_table(
-                Table::create()
-                    .table(Post::Table)
-                    .if_not_exists()
-                    .col(pk_auto(Post::Id))
-                    .col(string(Post::Title))
-                    .col(string(Post::Text))
-                    .to_owned(),
-            )
-            .await
+        let db = manager.get_connection();
+        let file_names = vec![
+            "00-create-posts-table.sql",
+        ];
+
+        if let Err(err) = execute_sql_scripts(db, UP_SQL_DIR, &file_names).await {
+            return Err(err);
+        }
+
+        Ok(())
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
+        let db = manager.get_connection();
+        let file_names = vec![
+            "00-drop-posts-table.sql",
+        ];
 
-        manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
-            .await
+        if let Err(err) = execute_sql_scripts(db, DOWN_SQL_DIR, &file_names).await {
+            return Err(err);
+        }
+
+        Ok(())
     }
 }
 
