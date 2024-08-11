4a6e2237afbb33a62f564833e9ece558831aee17
DSanoussy <sanoussy.dansso@stonal.com>
2024-08-11 09:46:06 +0200

    exploring things

--- src/db_connection.rs
+++ src/db_connection.rs
@@ -1,9 +1,12 @@
 use std::env;
 use std::time::Duration;
+use dotenv::dotenv;
 use sea_orm::{ConnectOptions, Database, DatabaseConnection, DbErr};
 
 pub async fn establish_connection() -> Result<DatabaseConnection, DbErr> {
-    let mut opt = ConnectOptions::new("postgres://postgres:postgres@localhost:5434/todolist");
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let mut opt = ConnectOptions::new(database_url);
     opt.max_connections(100)
         .min_connections(5)
         .connect_timeout(Duration::from_secs(8))
@@ -14,7 +17,5 @@ pub async fn establish_connection() -> Result<DatabaseConnection, DbErr> {
         .sqlx_logging_level(log::LevelFilter::Info)
         .set_schema_search_path("public"); // Setting default PostgreSQL schema
 
-    let db = Database::connect(opt).await;
-
-    db
+    Database::connect(opt).await
 }
