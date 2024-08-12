93f17eaf68b4f584d7ae1b3dbaa0468d4694d37b
DSanoussy <sanoussy.dansso@stonal.com>
2024-08-12 21:38:26 +0200

    interesting, could find better

--- src/db_connection.rs
+++ src/db_connection.rs
@@ -1,18 +1,14 @@
-use std::env;
-use std::time::Duration;
-use dotenv::dotenv;
 use sea_orm::{ConnectOptions, Database, DatabaseConnection, DbErr};
+use std::time::Duration;
+use crate::config::Config;
 
-pub async fn establish_connection() -> Result<DatabaseConnection, DbErr> {
-    dotenv().ok();
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let mut opt = ConnectOptions::new(database_url);
-    opt.max_connections(100)
-        .min_connections(5)
-        .connect_timeout(Duration::from_secs(8))
-        .acquire_timeout(Duration::from_secs(8))
-        .idle_timeout(Duration::from_secs(8))
-        .max_lifetime(Duration::from_secs(8))
+pub async fn establish_connection(config: &Config) -> Result<DatabaseConnection, DbErr> {
+    let mut opt = ConnectOptions::new(config.database_url.clone());
+
+    opt.max_connections(config.max_connections)
+        .min_connections(config.min_connections)
+        .connect_timeout(Duration::from_secs(config.connection_timeout))
+        .idle_timeout(Duration::from_secs(config.idle_timeout))
         .sqlx_logging(true)
         .sqlx_logging_level(log::LevelFilter::Info)
         .set_schema_search_path("public"); // Setting default PostgreSQL schema
