b203f8991b71a0aaed408c82d39905309ab2f4e8
DSanoussy <sanoussy.dansso@stonal.com>
2024-08-12 21:38:26 +0200

    interesting, could find better

--- src/main.rs
+++ src/main.rs
@@ -1,123 +1,38 @@
-use actix_web::{App, get, HttpResponse, HttpServer, post, Responder, web};
-use sea_orm::{ActiveModelTrait, DatabaseConnection, EntityTrait};
-use sea_orm::ActiveValue::Set;
-use entity::prelude::TodoLists;
-use serde::Deserialize;
-use entity::todo_lists;
-use crate::db_connection::establish_connection;
+use actix_web::{App, HttpServer, web};
+use sea_orm::DatabaseConnection;
+use crate::{
+    repositories::todo_repository::TodoListRepository,
+    db_connection::establish_connection,
+    services::todo_service::TodoListService,
+    config::Config
+};
+use crate::handlers::todo_handlers::{create_todo, get_todo_by_id, get_todos};
 
 mod db_connection;
-
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-#[get("/todos")]
-async fn get_todos(db: web::Data<DatabaseConnection>) -> impl Responder {
-    let todos = TodoLists::find().all(db.get_ref()).await.expect("Failed to fetch todos");
-    HttpResponse::Ok().json(todos)
-}
-
-#[get("/todos/{id}")]
-async fn get_todo_by_id(path: web::Path<i32>, db: web::Data<DatabaseConnection>) -> impl Responder {
-    let id = path.into_inner();
-    let todo = TodoLists::find_by_id(id).one(db.get_ref()).await;
-
-    match todo {
-        Ok(Some(todo)) => HttpResponse::Ok().json(todo),
-        Ok(None) => HttpResponse::NotFound().body("Todo not found"),
-        Err(_) => HttpResponse::InternalServerError().finish(),
-    }
-}
-
-#[derive(Deserialize, Debug)]
-pub struct TodoView {
-    pub title: String
-}
-
-#[derive(Clone, Debug)]
-pub struct TodoList {
-    pub title: String,
-}
-
-pub struct TodoListRepository {
-    db: DatabaseConnection,
-}
-
-impl TodoListRepository {
-    pub fn new(db: DatabaseConnection) -> Self {
-        TodoListRepository { db }
-    }
-
-    pub async fn save(&self, todo: TodoList) -> Result<(), sea_orm::DbErr> {
-        let new_todo = todo_lists::ActiveModel {
-            id: Default::default(),
-            title: Set(todo.title),
-        };
-
-        if let Err(err) = new_todo.insert(&self.db).await {
-            eprintln!("Error while saving todo : {:?}", err);
-            return Err(err);
-        }
-
-        Ok(())
-    }
-}
-
-impl TodoList {
-    pub fn new(title: String) -> Self {
-        Self { title }
-    }
-}
-
-pub struct TodoListService {
-    repository: TodoListRepository,
-}
-
-impl TodoListService {
-    pub fn new(repository: TodoListRepository) -> Self {
-        TodoListService { repository }
-    }
-
-    async fn create_todo(&self, dto: TodoView) -> Result<(), ()> {
-        let new_todo = TodoList::new(dto.title);
-        self.repository.save(new_todo).await.expect("TODO: panic message");
-        Ok(())
-    }
-}
-
-#[post("/todos")]
-async fn create_todo(service: web::Data<TodoListService>, payload: web::Json<TodoView>) -> impl Responder {
-    service.create_todo(payload.into_inner()).await.expect("TODO: panic message");
-    HttpResponse::Created().finish()
-}
+mod repositories;
+mod services;
+mod handlers;
+mod models;
+mod config;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // SEA ORM
-    let connection: DatabaseConnection = match establish_connection().await {
-        Ok(conn) => conn,
-        Err(err) => {
-            eprintln!("Error while connecting to the database : {:?}", err);
-            std::process::exit(1);
-        }
-    };
+    // Initialize configuration
+    let config = Config::from_env();
 
+    // Establish a database connection using the configuration
+    let connection: DatabaseConnection = establish_connection(&config)
+        .await
+        .expect("Failed to connect to the database");
+
+    // Initialize repositories and services
     let todo_repository = TodoListRepository::new(connection);
     let todo_service = web::Data::new(TodoListService::new(todo_repository));
 
-    // ACTIX
+    // Start the Actix Web server
     HttpServer::new(move || {
         App::new()
-            .app_data(todo_service.clone())// Use of `.clone()` to duplicate the connection for each Actix thread
-            .service(hello)
-            .service(echo)
+            .app_data(todo_service.clone())
             .service(get_todos)
             .service(get_todo_by_id)
             .service(create_todo)
@@ -125,46 +40,4 @@ async fn main() -> std::io::Result<()> {
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
-}
-
-#[cfg(test)]
-mod tests {
-    use actix_web::test;
-
-    use super::*;
-
-    mod actix_tests {
-        use actix_web::{App, test};
-        use actix_web::http::header::ContentType;
-
-        use crate::{echo, hello};
-
-        #[actix_web::test]
-        async fn test_hello_get() {
-            let app = test::init_service(App::new().service(hello)).await;
-            let req = test::TestRequest::default()
-                .insert_header(ContentType::plaintext())
-                .to_request();
-            let resp = test::call_service(&app, req).await;
-            assert!(resp.status().is_success());
-        }
-
-        #[actix_web::test]
-        async fn test_echo_post() {
-            let app = test::init_service(App::new().service(echo)).await;
-            let req = test::TestRequest::post().uri("/echo").to_request();
-            let resp = test::call_service(&app, req).await;
-            assert!(resp.status().is_success());
-        }
-
-        #[actix_web::test]
-        async fn test_echo_post_error() {
-            let app = test::init_service(App::new().service(echo)).await;
-            let req = test::TestRequest::post().uri("/").to_request();
-            let resp = test::call_service(&app, req).await;
-            assert!(resp.status().is_client_error());
-        }
-    }
-
-    // TODO tests SeaORM
 }
