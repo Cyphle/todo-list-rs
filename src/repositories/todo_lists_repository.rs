27adad9037e1b18279b03feaa004e8c00ca33311
Cyphle <cyril.phamle@gmail.com>
2024-09-19 10:47:40 +0200

    sea orm test

--- src/repositories/todo_lists_repository.rs
+++ src/repositories/todo_lists_repository.rs
@@ -2,7 +2,7 @@
 #[cfg(test)]
 mod tests {
     mod todo_lists_read {
-        use sea_orm::{DatabaseBackend, DbErr, MockDatabase};
+        use sea_orm::{DatabaseBackend, DbErr, MockDatabase, Transaction};
         use sea_orm::{EntityTrait};
 
         use entity::todo_lists::Entity as TodoLists;
@@ -31,28 +31,90 @@ mod tests {
             );
 
             // Checking transaction log
-            // assert_eq!(
-            //     db.into_transaction_log(),
-            //     [
-            //         Transaction::from_sql_and_values(
-            //             DatabaseBackend::Postgres,
-            //             r#"SELECT "cake"."id", "cake"."name" FROM "cake" LIMIT $1"#,
-            //             [1u64.into()]
-            //         ),
-            //         Transaction::from_sql_and_values(
-            //             DatabaseBackend::Postgres,
-            //             r#"SELECT "cake"."id", "cake"."name" FROM "cake""#,
-            //             []
-            //         ),
-            //         Transaction::from_sql_and_values(
-            //             DatabaseBackend::Postgres,
-            //             r#"SELECT "cake"."id" AS "A_id", "cake"."name" AS "A_name", "fruit"."id" AS "B_id", "fruit"."name" AS "B_name", "fruit"."cake_id" AS "B_cake_id" FROM "cake" LEFT JOIN "fruit" ON "cake"."id" = "fruit"."cake_id""#,
-            //             []
-            //         ),
-            //     ]
-            // );
+            assert_eq!(
+                db.into_transaction_log(),
+                [
+                    Transaction::from_sql_and_values(
+                        DatabaseBackend::Postgres,
+                        r#"SELECT "todo_lists"."id", "todo_lists"."title" FROM "todo_lists" WHERE "todo_lists"."id" = $1 LIMIT $2"#,
+                        [1.into(), 1u64.into()],
+                    ),
+                ]
+            );
 
             Ok(())
         }
     }
+
+    // mod todo_lists_write {
+    //     use sea_orm::{
+    //         entity::prelude::*, entity::*, tests_cfg::*,
+    //         DatabaseBackend, MockDatabase, MockExecResult, Transaction,
+    //     };
+    //
+    //     #[async_std::test]
+    //     async fn test_insert_cake() -> Result<(), DbErr> {
+    //         // Create MockDatabase with mock execution result
+    //         let db = MockDatabase::new(DatabaseBackend::Postgres)
+    //             .append_query_results([
+    //                 [cake::Model {
+    //                     id: 15,
+    //                     name: "Apple Pie".to_owned(),
+    //                 }],
+    //                 [cake::Model {
+    //                     id: 16,
+    //                     name: "Apple Pie".to_owned(),
+    //                 }],
+    //             ])
+    //             .append_exec_results([
+    //                 MockExecResult {
+    //                     last_insert_id: 15,
+    //                     rows_affected: 1,
+    //                 },
+    //                 MockExecResult {
+    //                     last_insert_id: 16,
+    //                     rows_affected: 1,
+    //                 },
+    //             ])
+    //             .into_connection();
+    //
+    //         // Prepare the ActiveModel
+    //         let apple = cake::ActiveModel {
+    //             name: Set("Apple Pie".to_owned()),
+    //             ..Default::default()
+    //         };
+    //
+    //         // Insert the ActiveModel into MockDatabase
+    //         assert_eq!(
+    //             apple.clone().insert(&db).await?,
+    //             cake::Model {
+    //                 id: 15,
+    //                 name: "Apple Pie".to_owned()
+    //             }
+    //         );
+    //
+    //         // If you want to check the last insert id
+    //         let insert_result = cake::Entity::insert(apple).exec(&db).await?;
+    //         assert_eq!(insert_result.last_insert_id, 16);
+    //
+    //         // Checking transaction log
+    //         assert_eq!(
+    //             db.into_transaction_log(),
+    //             [
+    //                 Transaction::from_sql_and_values(
+    //                     DatabaseBackend::Postgres,
+    //                     r#"INSERT INTO "cake" ("name") VALUES ($1) RETURNING "id", "name""#,
+    //                     ["Apple Pie".into()]
+    //                 ),
+    //                 Transaction::from_sql_and_values(
+    //                     DatabaseBackend::Postgres,
+    //                     r#"INSERT INTO "cake" ("name") VALUES ($1) RETURNING "id""#,
+    //                     ["Apple Pie".into()]
+    //                 ),
+    //             ]
+    //         );
+    //
+    //         Ok(())
+    //     }
+    // }
 }
