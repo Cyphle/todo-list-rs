d1218a29f9f3cef6cc6b1a5021c1ecba72cdd0ee
Cyphle <cyril.phamle@gmail.com>
2024-09-20 15:42:52 +0200

    static for tests

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use actix_web::{web, App, HttpServer, Responder};
-use sea_orm::{DatabaseConnection, DbErr, EntityTrait};
+use actix_web::Responder;
+use sea_orm::EntityTrait;
 
-use entity::todo_lists::Entity as TodoLists;
 mod config;
 mod repositories;
 mod dto;
@@ -10,30 +9,11 @@ mod http;
 
 use config::database_config;
 use http::handlers::examples::{echo, hello};
-use crate::http::handlers::state::HandlerState;
-use crate::http::handlers::todo_lists::{create_todo_list, get_todo_list_by_id, get_todo_lists};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // TODO il faut que ça soit static : https://doc.rust-lang.org/rust-by-example/scope/lifetime/static_lifetime.html
     let db = database_config::connect().await.unwrap();
+    let static_db = Box::leak(Box::new(db));
 
-    // config::actix::config(&db.unwrap()).await
-
-    // HttpServer::new(|| {
-    //     App::new()
-    //         .app_data(web::Data::new(HandlerState {
-    //             db_connection: &db
-    //         }))
-    //         .service(hello)
-    //         .service(echo)
-    //         .service(get_todo_lists)
-    //         .service(get_todo_list_by_id)
-    //         .service(create_todo_list)
-    // })
-    //     .bind(("127.0.0.1", 8080))?
-    //     .run()
-    //     .await
-
-    Ok(())
+    config::actix::config(static_db).await
 }
